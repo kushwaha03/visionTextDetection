0fdb6f9cfd31d824de36530d6707e87861e82e09
krishna <33350985+kushwaha03@users.noreply.github.com>
2020-09-12 16:45:42 +0530

    Initial commit

---
+++ visionTextDetection/ViewController.swift
@@ -0,0 +1,425 @@
+//
+//  ViewController.swift
+//  AutoDetectCard
+//
+//  Created by Krishna Kushwaha on 12/09/20.
+//  Copyright © 2020 Krishna Kushwaha. All rights reserved.
+//
+
+import UIKit
+import Photos
+import Vision
+import VisionKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    
+    @IBOutlet weak var imageView: UIImageView!
+
+    // Layer into which to draw bounding box paths.
+    var pathLayer: CALayer?
+    
+    // Image parameters for reuse throughout app
+    var imageWidth: CGFloat = 0
+    var imageHeight: CGFloat = 0
+    
+    // Background is black, so display status bar in white.
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    
+    lazy var textDetectionRequest: VNDetectTextRectanglesRequest = {
+        let textDetectRequest = VNDetectTextRectanglesRequest(completionHandler: self.handleDetectedText)
+        // Tell Vision to report bounding box around each character.
+        textDetectRequest.reportCharacterBoxes = true
+        return textDetectRequest
+    }()
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        // Tapping the image view brings up the photo picker.
+        let photoTap = UITapGestureRecognizer(target: self, action: #selector(promptPhoto))
+        self.view.addGestureRecognizer(photoTap)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if imageView.image == nil {
+            promptPhoto()
+        }
+    }
+    
+    @objc
+    func promptPhoto() {
+        
+        let prompt = UIAlertController(title: "Choose a Photo",
+                                       message: "Please choose a photo.",
+                                       preferredStyle: .actionSheet)
+        
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        
+        func presentCamera(_ _: UIAlertAction) {
+            imagePicker.sourceType = .camera
+            self.present(imagePicker, animated: true)
+        }
+        
+        let cameraAction = UIAlertAction(title: "Camera",
+                                         style: .default,
+                                         handler: presentCamera)
+        
+        func presentLibrary(_ _: UIAlertAction) {
+            imagePicker.sourceType = .photoLibrary
+            self.present(imagePicker, animated: true)
+        }
+        
+        let libraryAction = UIAlertAction(title: "Photo Library",
+                                          style: .default,
+                                          handler: presentLibrary)
+        
+        func presentAlbums(_ _: UIAlertAction) {
+            imagePicker.sourceType = .savedPhotosAlbum
+            self.present(imagePicker, animated: true)
+        }
+        
+        let albumsAction = UIAlertAction(title: "Saved Albums",
+                                         style: .default,
+                                         handler: presentAlbums)
+        
+        let cancelAction = UIAlertAction(title: "Cancel",
+                                         style: .cancel,
+                                         handler: nil)
+        
+        prompt.addAction(cameraAction)
+        prompt.addAction(libraryAction)
+        prompt.addAction(albumsAction)
+        prompt.addAction(cancelAction)
+        
+        self.present(prompt, animated: true, completion: nil)
+    }
+    
+    // MARK: - Helper Methods
+    
+    /// - Tag: PreprocessImage
+    func scaleAndOrient(image: UIImage) -> UIImage {
+        
+        // Set a default value for limiting image size.
+        let maxResolution: CGFloat = 640
+        
+        guard let cgImage = image.cgImage else {
+            print("UIImage has no CGImage backing it!")
+            return image
+        }
+        
+        // Compute parameters for transform.
+        let width = CGFloat(cgImage.width)
+        let height = CGFloat(cgImage.height)
+        var transform = CGAffineTransform.identity
+        
+        var bounds = CGRect(x: 0, y: 0, width: width, height: height)
+        
+        if width > maxResolution ||
+            height > maxResolution {
+            let ratio = width / height
+            if width > height {
+                bounds.size.width = maxResolution
+                bounds.size.height = round(maxResolution / ratio)
+            } else {
+                bounds.size.width = round(maxResolution * ratio)
+                bounds.size.height = maxResolution
+            }
+        }
+        
+        let scaleRatio = bounds.size.width / width
+        let orientation = image.imageOrientation
+        switch orientation {
+        case .up:
+            transform = .identity
+        case .down:
+            transform = CGAffineTransform(translationX: width, y: height).rotated(by: .pi)
+        case .left:
+            let boundsHeight = bounds.size.height
+            bounds.size.height = bounds.size.width
+            bounds.size.width = boundsHeight
+            transform = CGAffineTransform(translationX: 0, y: width).rotated(by: 3.0 * .pi / 2.0)
+        case .right:
+            let boundsHeight = bounds.size.height
+            bounds.size.height = bounds.size.width
+            bounds.size.width = boundsHeight
+            transform = CGAffineTransform(translationX: height, y: 0).rotated(by: .pi / 2.0)
+        case .upMirrored:
+            transform = CGAffineTransform(translationX: width, y: 0).scaledBy(x: -1, y: 1)
+        case .downMirrored:
+            transform = CGAffineTransform(translationX: 0, y: height).scaledBy(x: 1, y: -1)
+        case .leftMirrored:
+            let boundsHeight = bounds.size.height
+            bounds.size.height = bounds.size.width
+            bounds.size.width = boundsHeight
+            transform = CGAffineTransform(translationX: height, y: width).scaledBy(x: -1, y: 1).rotated(by: 3.0 * .pi / 2.0)
+        case .rightMirrored:
+            let boundsHeight = bounds.size.height
+            bounds.size.height = bounds.size.width
+            bounds.size.width = boundsHeight
+            transform = CGAffineTransform(scaleX: -1, y: 1).rotated(by: .pi / 2.0)
+        default:
+            transform = .identity
+        }
+        
+        return UIGraphicsImageRenderer(size: bounds.size).image { rendererContext in
+            let context = rendererContext.cgContext
+            
+            if orientation == .right || orientation == .left {
+                context.scaleBy(x: -scaleRatio, y: scaleRatio)
+                context.translateBy(x: -height, y: 0)
+            } else {
+                context.scaleBy(x: scaleRatio, y: -scaleRatio)
+                context.translateBy(x: 0, y: -height)
+            }
+            context.concatenate(transform)
+            context.draw(cgImage, in: CGRect(x: 0, y: 0, width: width, height: height))
+        }
+    }
+    
+    func presentAlert(_ title: String, error: NSError) {
+        // Always present alert on main thread.
+        DispatchQueue.main.async {
+            let alertController = UIAlertController(title: title,
+                                                    message: error.localizedDescription,
+                                                    preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK",
+                                         style: .default) { _ in
+                                            // Do nothing -- simply dismiss alert.
+            }
+            alertController.addAction(okAction)
+            self.present(alertController, animated: true, completion: nil)
+        }
+    }
+    
+    
+    // MARK: - Vision
+    
+    /// - Tag: PerformRequests
+    fileprivate func performVisionRequest(image: CGImage, orientation: CGImagePropertyOrientation) {
+        
+        // Fetch desired requests based on switch status.
+        let requests = createVisionRequests()
+        // Create a request handler.
+        let imageRequestHandler = VNImageRequestHandler(cgImage: image,
+                                                        orientation: orientation,
+                                                        options: [:])
+        
+        // Send the requests to the request handler.
+        DispatchQueue.global(qos: .userInitiated).async {
+            do {
+                try imageRequestHandler.perform(requests)
+            } catch let error as NSError {
+                print("Failed to perform image request: \(error)")
+                self.presentAlert("Image Request Failed", error: error)
+                return
+            }
+        }
+    }
+
+    
+    /// - Tag: CreateRequests
+    fileprivate func createVisionRequests() -> [VNRequest] {
+        
+        // Create an array to collect all desired requests.
+        var requests: [VNRequest] = []
+        
+//        // Create & include a request if and only if switch is ON.
+//        if self.rectSwitch.isOn {
+//            requests.append(self.rectangleDetectionRequest)
+//        }
+//        if self.faceSwitch.isOn {
+//            // Break rectangle & face landmark detection into 2 stages to have more fluid feedback in UI.
+//            requests.append(self.faceDetectionRequest)
+//            requests.append(self.faceLandmarkRequest)
+//        }
+//        if self.textSwitch.isOn {
+            requests.append(self.textDetectionRequest)
+        print(self.textDetectionRequest)
+//        }
+//        if self.barcodeSwitch.isOn {
+//            requests.append(self.barcodeDetectionRequest)
+//        }
+        
+        // Return grouped requests as a single array.
+        return requests
+    }
+    
+    // MARK: - Path-Drawing
+    
+    fileprivate func boundingBox(forRegionOfInterest: CGRect, withinImageBounds bounds: CGRect) -> CGRect {
+        
+        let imageWidth = bounds.width
+        let imageHeight = bounds.height
+        
+        // Begin with input rect.
+        var rect = forRegionOfInterest
+        
+        // Reposition origin.
+        rect.origin.x *= imageWidth
+        rect.origin.x += bounds.origin.x
+        rect.origin.y = (1 - rect.origin.y) * imageHeight + bounds.origin.y
+        
+        // Rescale normalized coordinates.
+        rect.size.width *= imageWidth
+        rect.size.height *= imageHeight
+        
+        return rect
+    }
+    
+    fileprivate func shapeLayer(color: UIColor, frame: CGRect) -> CAShapeLayer {
+        // Create a new layer.
+        let layer = CAShapeLayer()
+        
+        // Configure layer's appearance.
+        layer.fillColor = nil // No fill to show boxed object
+        layer.shadowOpacity = 0
+        layer.shadowRadius = 0
+        layer.borderWidth = 2
+        
+        // Vary the line color according to input.
+        layer.borderColor = color.cgColor
+        
+        // Locate the layer.
+        layer.anchorPoint = .zero
+        layer.frame = frame
+        layer.masksToBounds = true
+        
+        // Transform the layer to have same coordinate system as the imageView underneath it.
+        layer.transform = CATransform3DMakeScale(1, -1, 1)
+        
+        return layer
+    }
+    
+    // Lines of text are RED.  Individual characters are PURPLE.
+    fileprivate func draw(text: [VNTextObservation], onImageWithBounds bounds: CGRect) {
+        CATransaction.begin()
+        for wordObservation in text {
+            let wordBox = boundingBox(forRegionOfInterest: wordObservation.boundingBox, withinImageBounds: bounds)
+            let wordLayer = shapeLayer(color: .red, frame: wordBox)
+            
+            // Add to pathLayer on top of image.
+            pathLayer?.addSublayer(wordLayer)
+            
+            // Iterate through each character within the word and draw its box.
+            guard let charBoxes = wordObservation.characterBoxes else {
+                continue
+            }
+            for charObservation in charBoxes {
+                let charBox = boundingBox(forRegionOfInterest: charObservation.boundingBox, withinImageBounds: bounds)
+                let charLayer = shapeLayer(color: .purple, frame: charBox)
+                charLayer.borderWidth = 1
+                
+                // Add to pathLayer on top of image.
+                pathLayer?.addSublayer(charLayer)
+            }
+        }
+        CATransaction.commit()
+    }
+    
+    fileprivate func handleDetectedText(request: VNRequest?, error: Error?) {
+        if let nsError = error as NSError? {
+            self.presentAlert("Text Detection Error", error: nsError)
+            return
+        }
+        // Perform drawing on the main thread.
+        DispatchQueue.main.async {
+            guard let drawLayer = self.pathLayer,
+                let results = request?.results as? [VNTextObservation] else {
+                    return
+            }
+            for result in results {
+                print("boundingBox \(result.boundingBox)")
+                           print("characterBoxes \(result.characterBoxes)")        
+            }
+            print(results)
+            self.draw(text: results, onImageWithBounds: drawLayer.bounds)
+            drawLayer.setNeedsDisplay()
+        }
+    }
+    
+    
+    // MARK: - UIImagePickerControllerDelegate
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        // Dismiss picker, returning to original root viewController.
+        dismiss(animated: true, completion: nil)
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any])
+    {
+        // Extract chosen image.
+        let originalImage: UIImage = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        
+        // Display image on screen.
+        show(originalImage)
+        
+        // Convert from UIImageOrientation to CGImagePropertyOrientation.
+        let cgOrientation = CGImagePropertyOrientation(rawValue: UInt32(originalImage.imageOrientation.rawValue))
+        
+        // Fire off request based on URL of chosen photo.
+        guard let cgImage = originalImage.cgImage else {
+            return
+        }
+        performVisionRequest(image: cgImage, orientation: cgOrientation!)
+        
+        // Dismiss the picker to return to original view controller.
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func show(_ image: UIImage) {
+        
+        // Remove previous paths & image
+        pathLayer?.removeFromSuperlayer()
+        pathLayer = nil
+        imageView.image = nil
+        
+        // Account for image orientation by transforming view.
+        let correctedImage = scaleAndOrient(image: image)
+        
+        // Place photo inside imageView.
+        imageView.image = correctedImage
+        
+        // Transform image to fit screen.
+        guard let cgImage = correctedImage.cgImage else {
+            print("Trying to show an image not backed by CGImage!")
+            return
+        }
+        
+        let fullImageWidth = CGFloat(cgImage.width)
+        let fullImageHeight = CGFloat(cgImage.height)
+        
+        let imageFrame = imageView.frame
+        let widthRatio = fullImageWidth / imageFrame.width
+        let heightRatio = fullImageHeight / imageFrame.height
+        
+        // ScaleAspectFit: The image will be scaled down according to the stricter dimension.
+        let scaleDownRatio = max(widthRatio, heightRatio)
+        
+        // Cache image dimensions to reference when drawing CALayer paths.
+        imageWidth = fullImageWidth / scaleDownRatio
+        imageHeight = fullImageHeight / scaleDownRatio
+        
+        // Prepare pathLayer to hold Vision results.
+        let xLayer = (imageFrame.width - imageWidth) / 2
+        let yLayer = imageView.frame.minY + (imageFrame.height - imageHeight) / 2
+        let drawingLayer = CALayer()
+        drawingLayer.bounds = CGRect(x: xLayer, y: yLayer, width: imageWidth, height: imageHeight)
+        drawingLayer.anchorPoint = CGPoint.zero
+        drawingLayer.position = CGPoint(x: xLayer, y: yLayer)
+        drawingLayer.opacity = 0.5
+        pathLayer = drawingLayer
+        self.view.layer.addSublayer(pathLayer!)
+    }
+    
+}
+
